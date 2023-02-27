0f5def50aeb4f70b532ac37288906d8d18067cdc
zt222 <tao.zheng@aitos.io>
2023-02-27 11:19:02 +0800

    fix:
    resolve the redefinition of  ip4addr_aton

--- platform/Fibocom-L610/src/dal/http/httpclient.c
+++ platform/Fibocom-L610/src/dal/http/httpclient.c
@@ -155,142 +155,142 @@ unsigned short boat_htons(unsigned short n)
     return ((n & 0xff) << 8) | ((n & 0xff00) >> 8);
 }
 
-int ip4addr_aton(const char *cp, ip4_addr_t *addr)
-{
-    u32_t val;
-    u8_t base;
-    char c;
-    u32_t parts[4];
-    u32_t *pp = parts;
+// int ip4addr_aton(const char *cp, ip4_addr_t *addr)
+// {
+//     u32_t val;
+//     u8_t base;
+//     char c;
+//     u32_t parts[4];
+//     u32_t *pp = parts;
 
-    c = *cp;
-    for (;;)
-    {
-        /*
-         * Collect number up to ``.''.
-         * Values are specified as for C:
-         * 0x=hex, 0=octal, 1-9=decimal.
-         */
-        if (!isdigit(c))
-        {
-            return 0;
-        }
-        val = 0;
-        base = 10;
-        if (c == '0')
-        {
-            c = *++cp;
-            if (c == 'x' || c == 'X')
-            {
-                base = 16;
-                c = *++cp;
-            }
-            else
-            {
-                base = 8;
-            }
-        }
-        for (;;)
-        {
-            if (isdigit(c))
-            {
-                val = (val * base) + (u32_t)(c - '0');
-                c = *++cp;
-            }
-            else if (base == 16 && isxdigit(c))
-            {
-                val = (val << 4) | (u32_t)(c + 10 - (islower(c) ? 'a' : 'A'));
-                c = *++cp;
-            }
-            else
-            {
-                break;
-            }
-        }
-        if (c == '.')
-        {
-            /*
-             * Internet format:
-             *  a.b.c.d
-             *  a.b.c   (with c treated as 16 bits)
-             *  a.b (with b treated as 24 bits)
-             */
-            if (pp >= parts + 3)
-            {
-                return 0;
-            }
-            *pp++ = val;
-            c = *++cp;
-        }
-        else
-        {
-            break;
-        }
-    }
-    /*
-     * Check for trailing characters.
-     */
-    if (c != '\0' && !isspace(c))
-    {
-        return 0;
-    }
-    /*
-     * Concoct the address according to
-     * the number of parts specified.
-     */
-    switch (pp - parts + 1)
-    {
+//     c = *cp;
+//     for (;;)
+//     {
+//         /*
+//          * Collect number up to ``.''.
+//          * Values are specified as for C:
+//          * 0x=hex, 0=octal, 1-9=decimal.
+//          */
+//         if (!isdigit(c))
+//         {
+//             return 0;
+//         }
+//         val = 0;
+//         base = 10;
+//         if (c == '0')
+//         {
+//             c = *++cp;
+//             if (c == 'x' || c == 'X')
+//             {
+//                 base = 16;
+//                 c = *++cp;
+//             }
+//             else
+//             {
+//                 base = 8;
+//             }
+//         }
+//         for (;;)
+//         {
+//             if (isdigit(c))
+//             {
+//                 val = (val * base) + (u32_t)(c - '0');
+//                 c = *++cp;
+//             }
+//             else if (base == 16 && isxdigit(c))
+//             {
+//                 val = (val << 4) | (u32_t)(c + 10 - (islower(c) ? 'a' : 'A'));
+//                 c = *++cp;
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         if (c == '.')
+//         {
+//             /*
+//              * Internet format:
+//              *  a.b.c.d
+//              *  a.b.c   (with c treated as 16 bits)
+//              *  a.b (with b treated as 24 bits)
+//              */
+//             if (pp >= parts + 3)
+//             {
+//                 return 0;
+//             }
+//             *pp++ = val;
+//             c = *++cp;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+//     /*
+//      * Check for trailing characters.
+//      */
+//     if (c != '\0' && !isspace(c))
+//     {
+//         return 0;
+//     }
+//     /*
+//      * Concoct the address according to
+//      * the number of parts specified.
+//      */
+//     switch (pp - parts + 1)
+//     {
 
-    case 0:
-        return 0; /* initial nondigit */
+//     case 0:
+//         return 0; /* initial nondigit */
 
-    case 1: /* a -- 32 bits */
-        break;
+//     case 1: /* a -- 32 bits */
+//         break;
 
-    case 2: /* a.b -- 8.24 bits */
-        if (val > 0xffffffUL)
-        {
-            return 0;
-        }
-        if (parts[0] > 0xff)
-        {
-            return 0;
-        }
-        val |= parts[0] << 24;
-        break;
+//     case 2: /* a.b -- 8.24 bits */
+//         if (val > 0xffffffUL)
+//         {
+//             return 0;
+//         }
+//         if (parts[0] > 0xff)
+//         {
+//             return 0;
+//         }
+//         val |= parts[0] << 24;
+//         break;
 
-    case 3: /* a.b.c -- 8.8.16 bits */
-        if (val > 0xffff)
-        {
-            return 0;
-        }
-        if ((parts[0] > 0xff) || (parts[1] > 0xff))
-        {
-            return 0;
-        }
-        val |= (parts[0] << 24) | (parts[1] << 16);
-        break;
+//     case 3: /* a.b.c -- 8.8.16 bits */
+//         if (val > 0xffff)
+//         {
+//             return 0;
+//         }
+//         if ((parts[0] > 0xff) || (parts[1] > 0xff))
+//         {
+//             return 0;
+//         }
+//         val |= (parts[0] << 24) | (parts[1] << 16);
+//         break;
 
-    case 4: /* a.b.c.d -- 8.8.8.8 bits */
-        if (val > 0xff)
-        {
-            return 0;
-        }
-        if ((parts[0] > 0xff) || (parts[1] > 0xff) || (parts[2] > 0xff))
-        {
-            return 0;
-        }
-        val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
-        break;
-    default:
-        break;
-    }
-    if (addr)
-    {
-        ip4_addr_set_u32(addr, val);
-    }
-    return 1;
-}
+//     case 4: /* a.b.c.d -- 8.8.8.8 bits */
+//         if (val > 0xff)
+//         {
+//             return 0;
+//         }
+//         if ((parts[0] > 0xff) || (parts[1] > 0xff) || (parts[2] > 0xff))
+//         {
+//             return 0;
+//         }
+//         val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+//         break;
+//     default:
+//         break;
+//     }
+//     if (addr)
+//     {
+//         ip4_addr_set_u32(addr, val);
+//     }
+//     return 1;
+// }
 
 static int getHostByIpOrName(char *hostorip_str, GAPP_TCPIP_ADDR_T *addr)
 {
