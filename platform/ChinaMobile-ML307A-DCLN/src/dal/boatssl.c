5784e69fc6adcd24f8c70f603d97e86efe5978dd
zzqAlex <18621758039@163.com>
2023-04-05 18:11:36 +0800

    feat[ML307A][#1]: Modify boatssl.c

--- platform/ChinaMobile-ML307A-DCLN/src/dal/boatssl.c
+++ platform/ChinaMobile-ML307A-DCLN/src/dal/boatssl.c
@@ -21,6 +21,8 @@
 #include "cm_asocket.h"
 #include "stdlib.h"
 
+#define SSL_ID  0
+
 // #if (PROTOCOL_USE_HLFABRIC == 1 || PROTOCOL_USE_CHAINMAKER_V1 == 1 || PROTOCOL_USE_CHAINMAKER_V2 == 1 || BOAT_PROTOCOL_USE_HWBCS == 1)
 /******************************************************************************
                               BOAT SOCKET WARPPER
@@ -101,103 +103,77 @@ BOAT_RESULT boat_find_subject_common_name(const BCHAR *cert, const BUINT32 certl
 *  for details.
 ****************************************************************************************
 */
+
+
 BSINT32 BoatConnect(const BCHAR *address, void *rsvd)
 {
-    int ret;
-    int sockFd;
-    char ip[64];
-    char port[8];
-    char *ptr = NULL;
-    // struct hostent *he;
-    // struct sockaddr_in server;
-    // struct sockaddr localaddr;
-    // struct sockaddr_in *localaddr_ptr;
-    // socklen_t addrlen = sizeof(struct sockaddr);
+	int connectfd;
+	char ip[64];
+	char port[8];
+	char *ptr = NULL;
+	// struct hostent *he;
+	struct sockaddr_in server;
+	struct sockaddr localaddr;
+	struct sockaddr_in *localaddr_ptr;
+	socklen_t addrlen = sizeof(struct sockaddr);
 
-    (void)rsvd;
+	(void)rsvd;
 
-    ptr = strchr(address, ':');
-    if (NULL == ptr)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "invalid address:%s.", address);
-        return -1;
-    }
+	ptr = strchr(address, ':');
+	if (NULL == ptr)
+	{
+		BoatLog(BOAT_LOG_CRITICAL, "invalid address:%s.", address);
+		return -1;
+	}
 
-    memset(ip, 0, sizeof(ip));
-    memset(port, 0, sizeof(port));
-    memcpy(ip, address, (int)(ptr - address));
-    memcpy(port, ptr + 1, strlen(address) - (int)(ptr - address));
+	memset(ip, 0, sizeof(ip));
+	memset(port, 0, sizeof(port));
+	memcpy(ip, address, (int)(ptr - address));
+	memcpy(port, ptr + 1, strlen(address) - (int)(ptr - address));
 
-#if (BOAT_TLS_SUPPORT == 1)
-   // connectfd = fibo_ssl_sock_create();
-    void *sslCtx = NULL;
-    sockFd = cm_asocket_open(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, NULL);
-   
-    ret = cm_ssl_conn(&sslCtx,0,sockFd,5000);
-    if (ret != 0)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "ssl connect() error ,code = %d", ret);
-        return -1;
-    }
-    /// get ssl socket fd
-   // int fd = fibo_ssl_sock_get_fd(connectfd);
+	// if ((he = gethostbyname(ip)) == NULL)
+	// {
+	// 	BoatLog(BOAT_LOG_CRITICAL, "gethostbyname() error");
+	// 	return -1;
+	// }
 
-    // set sock fd to unblock
-    lwip_fcntl(sockFd, F_SETFL, lwip_fcntl(sockFd, F_GETFL, 0) | O_NONBLOCK);
-    BoatLog(BOAT_LOG_VERBOSE, "%s:%s[%d] connected!", ip, port, sockFd);
+	if ((connectfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		BoatLog(BOAT_LOG_CRITICAL, "socket() error");
+		return -1;
+	}
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    server_addr.sin_port = htons(atoi(port));
+	struct timeval timeout = {0, 500 * 1000};
+	setsockopt(connectfd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(struct timeval));
+	setsockopt(connectfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval));
 
-    
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(port));
+	server.sin_addr.s_addr = inet_addr(ip);
 
-    ret = cm_asocket_connect(sockFd,(const struct sockaddr *)&server_addr,sizeof(server_addr));
-    if(ret != 0)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "asocket connect() error ,code = %d", ret);
-        return -1;
-    }
+	if (connect(connectfd, (struct sockaddr *)&server, sizeof(struct sockaddr)) < 0)
+	{
+		BoatLog(BOAT_LOG_CRITICAL, "connect() error");
+		close(connectfd);
+		return -1;
+	}
+	if (getsockname(connectfd, &localaddr, &addrlen) < 0)
+	{
+		BoatLog(BOAT_LOG_CRITICAL, "getsockname() error");
+		close(connectfd);
+		return -1;
+	}
+	else
+	{
+		localaddr_ptr = (struct sockaddr_in *)&localaddr;
+		BoatLog(BOAT_LOG_VERBOSE, "localIP: %s:%d.",
+				inet_ntoa(localaddr_ptr->sin_addr), htons(localaddr_ptr->sin_port));
+	}
 
+	BoatLog(BOAT_LOG_VERBOSE, "%s:%s[%d] connected!", ip, port, connectfd);
 
-#else
-    GAPP_TCPIP_ADDR_T addr;
-    if (getHostByIpOrName(address, &addr) != 0)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "getaddrinfo != 0, return HTTPCLIENT_UNRESOLVED_DNS");
-        return HTTPCLIENT_UNRESOLVED_DNS;
-    }
-
-    addr.sin_port = boat_htons(/*7545*/ client->remote_port);
-    if (client->socket < 0)
-    {
-        return HTTPCLIENT_ERROR_CONN;
-    }
-
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("120.27.12.119");
-    server_addr.sin_port = htons(2004);
-
-    if ((sockFd = cm_asocket_open(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, NULL)) < 0)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "socket() error");
-        return -1;
-    }
-
-    if (fibo_sock_connect(sockFd, &addr) == 0)
-    {
-        BoatLog(BOAT_LOG_CRITICAL, "connect() error");
-        fibo_ssl_sock_close(sockFd);
-        return -1;
-    }
-#endif
-
-
-    return sockFd;
+	return connectfd;
 }
 
 #if (BOAT_TLS_SUPPORT == 1)
@@ -234,24 +210,54 @@ BOAT_RESULT BoatTlsInit(const BCHAR *address, const BCHAR *hostName, const BoatF
 {
 
     int ret = 0;
+    void *sslCtx = NULL;
+
+    int verify_mode = 1;
+    int timeOut = 60;
+
+    BoatLog(BOAT_LOG_NORMAL, "CA Cert len = %d",caChain.field_len);
+    BoatLog(BOAT_LOG_NORMAL, "CA Cert=>%s",caChain.field_ptr+(caChain.field_len - 20));
+
     
-    int verify_mode = 2;
-    cm_ssl_setopt(0,CM_SSL_PARAM_VERIFY,&verify_mode);
-    cm_ssl_setopt(0,CM_SSL_PARAM_CA_CERT,caChain.field_ptr);
-    if (BOAT_TLS_IDENTIFY_CLIENT == 1)
-    {
-        cm_ssl_setopt(0,CM_SSL_PARAM_CLI_CERT,clientCert.field_ptr);
-        cm_ssl_setopt(0,CM_SSL_PARAM_CLI_KEY,clientPrikey.field_ptr);
-    }
-    
+    // BoatSleepMs(200);
 
     ret = BoatConnect(address, NULL);
-    if (ret == -1)
+    if (ret < 0)
     {
         BoatLog(BOAT_LOG_NORMAL, "socket connect fail ");
         return BOAT_ERROR;
     }
     *socketfd = ret;
+
+
+    BoatSleepMs(200);
+
+    if(BOAT_TLS_IDENTIFY_CLIENT == 1)
+    {
+        verify_mode = 2;
+    }
+
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CA_CERT_DELETE,NULL);
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CLI_CERT_DELETE,NULL);
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CLI_KEY_DELETE,NULL);
+
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_VERIFY,&verify_mode);
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_NEGOTIME,&timeOut);
+    cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CA_CERT,caChain.field_ptr);
+
+    if (BOAT_TLS_IDENTIFY_CLIENT == 1)
+    {
+        cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CLI_CERT,clientCert.field_ptr);
+        cm_ssl_setopt(SSL_ID,CM_SSL_PARAM_CLI_KEY,clientPrikey.field_ptr);
+    }
+
+    ret = cm_ssl_conn(&sslCtx,SSL_ID,*socketfd,5000);
+    if(ret != 0)
+    {
+        BoatLog(BOAT_LOG_NORMAL, " BoatTlsInit Failed,ret = %d",ret);
+        return BOAT_ERROR;
+    }
+    *tlsContext = sslCtx;
     // fibo_taskSleep(10000);
     BoatLog(BOAT_LOG_NORMAL, " BoatTlsInit OK");
     return BOAT_SUCCESS;
@@ -284,67 +290,14 @@ BSINT32 BoatSend(BSINT32 sockfd, boatSSlCtx *tlsContext, const BUINT8 *buf, size
     //! @todo BOAT_TLS_SUPPORT implementation in crypto default.
     //! @todo BOAT_HLFABRIC_TLS_SUPPORT implementation in crypto default.
 
-    int ret = cm_asocket_send(sockfd, buf, len,0);
-    // BoatLog(BOAT_LOG_VERBOSE, "write ssl send = %d ", ret);
+    int ret = cm_ssl_write(tlsContext,buf,len);
+    BoatLog(BOAT_LOG_VERBOSE, "write ssl send = %d ", ret);
     return ret;
 #else
     return cm_asocket_send(sockfd, buf, len, 0);
 #endif
 }
 
-static int ssl_recv_unblock(INT32 sock, void *buf, INT32 size, INT32 timeout)
-{
-    struct timeval tm = {0};
-    fd_set rset;
-    // BUINT8 *temp = buf;
-
-    //int fd = fibo_ssl_sock_get_fd(sock);
-
-    //int ret = fibo_ssl_sock_recv(sock, buf, size);
-    int ret = cm_asocket_recv(sock, buf, size, 0);
-    if (ret > 0)
-    {
-        // BoatLog(BOAT_LOG_VERBOSE, "recv data size:%d", ret);
-        return ret;
-    }
-    else if (ret < 0)
-    {
-        // BoatLog(BOAT_LOG_VERBOSE, "recv data fail");
-        return ret;
-    }
-
-    FD_ZERO(&rset);
-    FD_SET(sock, &rset);
-    tm.tv_sec = timeout / 1000;
-    tm.tv_usec = (timeout % 1000) * 1000;
-
-    ret = select(sock + 1, &rset, NULL, NULL, timeout > 0 ? &tm : NULL);
-    if (ret < 0)
-    {
-        BoatLog(BOAT_LOG_VERBOSE, "select failed:%s", strerror(errno));
-        return -1;
-    }
-    else if (ret == 0)
-    {
-        BoatLog(BOAT_LOG_VERBOSE, "select timeout");
-        return -1;
-    }
-    else
-    {
-        ret = cm_asocket_recv(sock, buf, size, 0);
-        if (ret >= 0)
-        {
-            // BoatLog(BOAT_LOG_VERBOSE, "recv data size:%d", ret);
-            return ret;
-        }
-        else if (ret < 0)
-        {
-            BoatLog(BOAT_LOG_VERBOSE, "recv data fail");
-            return ret;
-        }
-    }
-    return -1;
-}
 
 /**
 ****************************************************************************************
@@ -370,7 +323,9 @@ BSINT32 BoatRecv(BSINT32 sockfd, boatSSlCtx *tlsContext, BUINT8 *buf, size_t len
 {
 #if (BOAT_TLS_SUPPORT == 1)
     //! @todo BOAT_TLS_SUPPORT implementation in crypto default.
-    BSINT32 ret = recv(sockfd, buf, len, 0);
+
+    int ret = cm_ssl_read(tlsContext,buf,len);
+    BoatLog(BOAT_LOG_VERBOSE, "write ssl recv = %d ", ret);
     return ret;
 #else
     return recv(sockfd, buf, len, 0);
@@ -395,8 +350,9 @@ void BoatClose(BSINT32 sockfd, boatSSlCtx **tlsContext, void *rsvd)
 {
     // close(sockfd);
 #if (BOAT_TLS_SUPPORT == 1)
-    // free tls releated
-    //! @todo BOAT_TLS_SUPPORT implementation in crypto default.
+
+
+    cm_ssl_close(tlsContext);
     close(sockfd);
 #else
     close(sockfd);
