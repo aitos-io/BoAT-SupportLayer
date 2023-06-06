bbbb4bbe843808640beda83201802dc79cc2470a
penghaoaitos <hao.peng@aitos.io>
2023-06-06 13:59:31 +0800

    Clear compilation warnings for testuartsuite and testapp

--- tests/platform/linux-default/testapp.c
+++ tests/platform/linux-default/testapp.c
@@ -1,49 +1,9 @@
 #include <stdio.h>
 #include "bsltestentry.h"
 #include "boatlog.h"
-typedef int (* testfun)(void);
-
-static void runtest(char *name,testfun p)
-{
-    if(p == NULL)
-    {
-        BoatLog(BOAT_LOG_NORMAL, "=== test Error test function is NULL ===\n");
-    return;
-    }
-    if(p == NULL)
-    {
-        BoatLog(BOAT_LOG_NORMAL, "=== test Error test name is NULL ===\n");
-        return;
-    }    
-    BoatLog(BOAT_LOG_NORMAL, "=== start run boat %s tests ===\n",name);
-
-    int failed_number = p();
-
-    BoatLog(BOAT_LOG_NORMAL, "boat %s tests run completed,failed number:%d\n",name,failed_number);
-
-    if(failed_number > 0)
-    {
-        BoatLog(BOAT_LOG_NORMAL, "=== %s test failed === ",name);
-        return;
-    }
-    BoatLog(BOAT_LOG_NORMAL, "=== complete boat %s tests ===\n\n",name);
-}
-
 
 int main(int argc, char *argv[])
 {
-#if 0
-    BoatLog(BOAT_LOG_NORMAL, "===== linux-default tests =====\n");
-    //runtest("Queue", runQueueTests);
-    //runtest("Timer", runTimerTests);
-    //runtest("Uart", runUartTests);
-    //runtest("sem", runSemTests);
-    //runtest("mutex",runMutexTests);
-    //runtest("Task/Thread", runTaskTests);
-    //runtest("KeystoreSoft",runKeystoreSoftTests);
-    //runtest("KeystoreIntf",runKeystoreIntfTests);
-    //runtest("Keypair",runKeypairTests);
-#endif
     SRunner *sr = NULL;
     int failed_number = 0;
 
