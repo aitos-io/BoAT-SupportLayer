8af56fc6c444009b7c2da124a6dfb753be04613e
penghaoaitos <hao.peng@aitos.io>
2023-08-02 17:05:32 +0800

    fix[testtimersuite]:Replace 'sleep' with 'withBoatSleepMs' in testtimersuite.c

--- platform/include/boatconfig.h
+++ platform/include/boatconfig.h
@@ -1,18 +1,19 @@
 /******************************************************************************
- * Copyright (C) 2018-2021 aitos.io
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *****************************************************************************/
+* Copyright (C) 2018-2021 aitos.io
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*****************************************************************************/
+
 
 /****************************************************************************
  THIS FILE IS AUTO GENERATED FOR SPECIAL PLATFORM linux-default
@@ -27,10 +28,10 @@
 #define  PROTOCOL_USE_PLATON     0
 #define  PROTOCOL_USE_PLATONE     0
 #define  PROTOCOL_USE_FISCOBCOS     0
-#define  PROTOCOL_USE_HLFABRIC     0
+#define  PROTOCOL_USE_HLFABRIC     1
 #define  PROTOCOL_USE_HWBCS     0
 #define  PROTOCOL_USE_CHAINMAKER_V1     0
-#define  PROTOCOL_USE_CHAINMAKER_V2     0
+#define  PROTOCOL_USE_CHAINMAKER_V2     1
 #define  PROTOCOL_USE_VENACHAIN     1
 #define  PROTOCOL_USE_QUORUM     0
 #define  PROTOCOL_USE_CITA     0
